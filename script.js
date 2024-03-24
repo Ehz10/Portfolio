20ee6bbb2aa62ccabcadce0ae1a5658f5990784a
Tiago Rodrigues <80887201+Ehz10@users.noreply.github.com>
2024-03-24 20:17:05 +0000

    update

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function openNav(){
-    document.getElementById("MySideBar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("MySideBar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((element) => observer.observe(element));
-
-
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
@@ -39,20 +16,3 @@ function reveal() {
   
   window.addEventListener("scroll", reveal);
 reveal();
-
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-
-window.onscroll = function() {scrollFunction()};
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-                document.getElementById("back-to-top").style.display = "block";
-            } else {
-                document.getElementById("back-to-top").style.display = "none";
-            }
-        }
